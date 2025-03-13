f0f7c1a62cb8711e12201de6a0882195e8bd9a36
danik <daniilkashitsyn30@gmail.com>
2025-03-13 17:46:22 +0300

    add a swagger into test-branch

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -3,13 +3,48 @@ package main
 import (
 	"backend/internal/database"
 	"fmt"
+	"net/http"
+
+	_ "backend/docs"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @titile React + Go Orders tracker
+
+// GetBooks godoc
+// @Summary Получить список книг
+// @Description Возвращает все книги из базы данных
+// @Tags books
+// @Accept  json
+// @Produce  json
+// @Success 200 {array} models.Book
+// @Failure 500 {object} map[string]string
+// @Router /api/books [get]
 func main() {
 	err := database.InitDb()
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	database.GetBooks()
+	router := gin.Default()
+
+	router.Group("/*")
+	{
+		router.GET("/books", getBooks)
+		router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	}
+
+	router.Run(":8080")
+}
+
+func getBooks(c *gin.Context) {
+	books, err := database.GetBooks()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+	c.JSON(http.StatusOK, books)
 }
